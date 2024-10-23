e736b5322fa4924a6b5b5f82254c6d11cf10ec5f
Whatdoyumin <lym4963@naver.com>
2024-10-24 05:13:17 +0900

    style: Global font 추가 및 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Router from './routes/Router';
+import GlobalFont from './styles/GlobalFont';
 import GloabalStyle from './styles/GlobalStyle';
+import Router from './routes/Router';
 
 function App() {
   return (
     <>
+      <GlobalFont />
       <GloabalStyle />
       <Router />
     </>
