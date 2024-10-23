1afc6cd3caef8ffdf791e58640d4797302510fd5
Whatdoyumin <lym4963@naver.com>
2024-10-24 02:30:19 +0900

    style: 컨텐츠 크기 스타일 코드 제거

--- src/layout/root-layout.style.js
+++ src/layout/root-layout.style.js
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  backgroud-color: white;
-  max-width: 393px;
-  min-height: 100vh;
-  margin: 0 auto;
-  backgroud-color: white;
-  box-shadow: rgba(100, 100, 100, 0.2) 0 0 29px 0;
   display: flex;
   flex-direction: column;
 `;
