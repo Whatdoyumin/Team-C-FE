681de117e55d835c5bd20fbaa1a9002bb76b2498
Whatdoyumin <lym4963@naver.com>
2024-10-24 05:20:03 +0900

    style: 하단바 스타일링 적용

---
+++ src/components/navBar/NavBar.style.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const BottomNav = styled.nav`
+  max-width: var(--size-max-width);
+  width: 100%;
+  height: var(--size-navbar);
+  overflow: hidden;
+  position: fixed;
+  bottom: 0;
+  margin-left: calc(-1 * var(--size-side-gap));
+  box-shadow: 0 -5px 10px -5px rgba(0, 0, 0, 0.1);
+`;
+
+const NavDiv = styled.div`
+  padding: 14px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const NavItem = styled.div`
+  width: 52px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  color: ${(props) =>
+    props.selected ? 'var(--color-blue-main)' : 'var(--color-gray-main)'};
+
+  svg {
+    width: 1.8rem;
+    height: 1.8rem;
+  }
+
+  span {
+    white-space: nowrap;
+    font-size: 14px;
+  }
+`;
+
+export { BottomNav, NavDiv, NavItem };
