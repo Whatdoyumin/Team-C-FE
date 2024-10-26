a0f6bc2ebb78d0b3cae21973a4dccc2a24f47990
김연진 <kyj0719@gmail.com>
2024-10-27 00:01:31 +0900

    FEAT: banner 기능 구현 및 임시 이미지 추가, 패키지 추가

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,16 +1,68 @@
+// Banner.js
 import * as S from './banner.style';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Banner = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyLoad: true,
+    arrows: false,
+  };
+
   return (
-    <S.Container>
-      <S.Texts>
-        <S.Title>주택청약</S.Title>
-        <S.Content>
-          청약통장에 가입한 사람에 한하여 아파트 분양 시 당첨자를 선정
-        </S.Content>
-      </S.Texts>
-      <S.Img src="src/components/banner/img.png"></S.Img>
-    </S.Container>
+    <S.StyledSlider {...settings}>
+      <S.Banner>
+        <S.Texts>
+          <S.Title>주택청약</S.Title>
+          <S.Content>
+            청약통장에 가입한 사람에 한하여 아파트 분양 시 당첨자를 선정
+          </S.Content>
+        </S.Texts>
+        <S.Img src="src/components/banner/주택청약.png" />
+      </S.Banner>
+      <S.Banner>
+        <S.Texts>
+          <S.Title>청년도약계좌</S.Title>
+          <S.Content>
+            월 70만원을 5년 납입하면 약 5,000만 원을 드려요!
+          </S.Content>
+        </S.Texts>
+        <S.Img src="src/components/banner/청년도약계좌.png" />
+      </S.Banner>
+      <S.Banner>
+        <S.Texts>
+          <S.Title>N분위란?</S.Title>
+          <S.Content>
+            소득이나 비율을 기준으로 전체를 나누어 분류하는 방법
+          </S.Content>
+        </S.Texts>
+        <S.Img src="src/components/banner/분위.png" />
+      </S.Banner>
+      <S.Banner>
+        <S.Texts>
+          <S.Title>국가장학금</S.Title>
+          <S.Content>
+            등록금 부담을 줄이기 위해 소득 수준에 따라 차등 지원
+          </S.Content>
+        </S.Texts>
+        <S.Img src="src/components/banner/국가장학금.png" />
+      </S.Banner>
+      <S.Banner>
+        <S.Texts>
+          <S.Title>내일배움카드</S.Title>
+          <S.Content>
+            직업 능력을 향상시키기 위해 국가에서 훈련비를 지원하는 제도
+          </S.Content>
+        </S.Texts>
+        <S.Img src="src/components/banner/내일배움카드.png" />
+      </S.Banner>
+    </S.StyledSlider>
   );
 };
 
--- src/components/banner/banner.style.js
+++ src/components/banner/banner.style.js
@@ -1,7 +1,45 @@
 import styled from 'styled-components';
+import Slider from 'react-slick';
 
-const Container = styled.div`
-  display: flex;
+const StyledSlider = styled(Slider)`
+  width: 345px;
+  .slick-slide {
+    display: flex;
+    justify-content: center;
+  }
+
+  .slick-dots {
+    bottom: 5px;
+    display: flex;
+    justify-content: center;
+  }
+
+  .slick-dots ul {
+    display: flex;
+    width: 100%;
+  }
+
+  .slick-dots li {
+    width: 5px;
+  }
+
+  .slick-dots li button:before {
+    opacity: 1;
+    color: #ffffff;
+  }
+
+  .slick-dots li.slick-active button:before {
+    opacity: 1;
+    color: #e28958;
+  }
+`;
+
+const Banner = styled.div`
+  display: flex !important;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  min-width: 345px;
   min-height: 150px;
   border-radius: 20px;
   background-color: #53565d;
@@ -12,40 +50,35 @@ const Container = styled.div`
     rgba(149, 181, 237, 0.1),
     rgba(83, 86, 93, 1)
   );
-  background-origin: border-box;
   background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
 `;
-
 const Texts = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 60%;
-  justify-content: center;
-  margin-left: 20px;
-  gap: 10px;
+  display: flex; /* Flexbox 사용 */
+  flex-direction: column; /* 세로 정렬 유지 */
+  width: 60%; /* 폭을 조정하여 비율을 맞춤 */
+  justify-content: center; /* 수직 중앙 정렬 */
+  margin-left: 20px; /* 여백 설정 */
+  gap: 10px; /* 요소 간 간격 설정 */
 `;
 
 const Title = styled.div`
-  display: flex;
   color: #ffffff;
   font-size: 26px;
   font-weight: bold;
 `;
 
 const Content = styled.div`
-  display: flex;
   font-size: 15px;
   color: #ffffff;
   word-break: keep-all;
-  font-weight: semi-bold;
+  font-weight: 600;
+  line-height: 135%;
 `;
 
 const Img = styled.img`
-  display: flex;
-  width: 104px;
+  width: 120px;
   object-fit: cover;
+  margin-right: 10px;
 `;
 
-export { Container, Texts, Title, Content, Img };
+export { Texts, Title, Content, Img, Banner, StyledSlider };
