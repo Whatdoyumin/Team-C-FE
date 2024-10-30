182c871a46a3d969907c8fd54f181101544228a0
Whatdoyumin <lym4963@naver.com>
2024-10-30 16:44:31 +0900

    feat: 폼의 토글 버튼 그룹 옵션을 상수 배열로 설정

---
+++ src/constants/form_menu.js
@@ -0,0 +1,50 @@
+const EDUCATION = ['고등학교 졸업 이하', '대학교 재학', '대학교 졸업'];
+
+const FIELD = ['IT', '공학', '자연', '경상', '인문', '예체능'];
+
+const REGION = [
+  '서울',
+  '부산',
+  '대구',
+  '인천',
+  '광주',
+  '대전',
+  '울산',
+  '경기',
+  '강원',
+  '충북',
+  '충남',
+  '전북',
+  '전남',
+  '경북',
+  '경남',
+  '세종',
+  '제주',
+];
+
+const INTERESTS = ['일자리', '주거', '교육', '복지 / 문화', '참여 / 권리'];
+
+const FORM_MENU = [
+  {
+    id: 1,
+    title: '학력',
+    options: EDUCATION,
+  },
+  {
+    id: 2,
+    title: '분야',
+    options: FIELD,
+  },
+  {
+    id: 3,
+    title: '지역',
+    options: REGION,
+  },
+  {
+    id: 4,
+    title: '관심있는 키워드',
+    options: INTERESTS,
+  },
+];
+
+export { FORM_MENU };
