5d496778e81e7a948911fc8e124daa6f3c0fadea
Whatdoyumin <lym4963@naver.com>
2024-10-30 14:49:04 +0900

    style: header 레이아웃 변경에 따른 스타일 수정

--- src/components/header/Header.style.js
+++ src/components/header/Header.style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const Header = styled.div`
+const Header = styled.header`
   max-width: var(--size-max-width);
   width: 100%;
   height: var(--size-header);
@@ -11,15 +11,43 @@ const Header = styled.div`
   margin-left: calc(-1 * var(--size-side-gap));
   display: flex;
   justify-content: center;
+  align-items: end;
+  padding: 0 var(--size-side-gap);
+`;
+
+const Nav = styled.nav`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
   align-items: center;
+  border-bottom: 1px solid var(--color-gray-300);
+  position: relative;
+
+  svg {
+    width: 2rem;
+    height: 2rem;
+    color: var(--color-gray-700);
+  }
+`;
+
+const Profile = styled.div`
+  width: 28px;
+  height: 28px;
+  border-radius: 14px;
+  background-color: var(--color-gray-400);
 `;
 
 const Logo = styled(Link)`
-  font-size: 24px;
-  color: var(--color-blue-main);
+  font-size: 28px;
+  color: var(--color-blue-700);
   font-weight: 700;
   cursor: pointer;
   text-decoration: none;
+  position: absolute;
+  margin-left: calc(
+    (var(--size-max-width) - var(--size-logo)) / 2 - var(--size-side-gap)
+  );
 `;
 
-export { Header, Logo };
+export { Header, Nav, Logo, Profile };
