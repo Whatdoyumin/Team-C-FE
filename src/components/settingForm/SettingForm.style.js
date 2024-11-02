60138ef4079be1c2193f199a7d31d37f39a338ba
Whatdoyumin <lym4963@naver.com>
2024-11-02 21:52:12 +0900

    style: 정보 관리 폼프로필 이미지 내 태그 스타일링

--- src/components/settingForm/SettingForm.style.js
+++ src/components/settingForm/SettingForm.style.js
@@ -5,8 +5,8 @@ const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 26px;
-  padding: 0 12px;
+  gap: 22px;
+  padding: 0 6px 0 12px;
 `;
 
 const FormTitle = styled.p`
@@ -20,6 +20,29 @@ const ProfileImg = styled.div`
   height: 66px;
   border-radius: 33px;
   background-color: var(--color-gray-300);
+  position: relative;
+
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 1.5rem;
+    height: 1.5rem;
+    color: var(--color-gray-500);
+  }
+
+  img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: 33px;
+    object-fit: cover;
+  }
+`;
+
+const FileInput = styled.input`
+  display: none;
 `;
 
 const Section = styled.div`
@@ -47,4 +70,15 @@ const Input = styled.input`
   }
 `;
 
-export { Form, FormTitle, ProfileImg, Section, Input };
+const SubmitBtn = styled.button`
+  width: 100%;
+  background-color: var(--color-blue-700);
+  color: white;
+  font-size: 12px;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 15px;
+  margin-bottom: 24px;
+`;
+
+export { Form, FormTitle, ProfileImg, FileInput, Section, Input, SubmitBtn };
