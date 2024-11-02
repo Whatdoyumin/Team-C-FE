f7d414b2bdef6bf2614577c8a7916a70c85ad02b
Whatdoyumin <lym4963@naver.com>
2024-11-02 21:52:42 +0900

    feat: 폼 섹션의 부연 설명 기능 추가

--- src/components/formSection/FormSection.jsx
+++ src/components/formSection/FormSection.jsx
@@ -1,11 +1,12 @@
 import * as S from './FormSection.style';
 
-const FormSection = ({ title, isOptional, children }) => {
+const FormSection = ({ title, isOptional, additional, children }) => {
   return (
     <S.Section>
       <S.SectionTitle>
         <S.Title>{title}</S.Title>
-        <S.Asterisk>{isOptional ? null : '*'}</S.Asterisk>
+        {!isOptional && <S.Asterisk>*</S.Asterisk>}
+        {additional && <S.Additional>{additional}</S.Additional>}
       </S.SectionTitle>
       {children}
     </S.Section>
