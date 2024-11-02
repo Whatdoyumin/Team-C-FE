1fb7fc9b82a17f4238126e9cfe048649b6f92e26
Whatdoyumin <lym4963@naver.com>
2024-11-02 21:54:14 +0900

    feat: 정보 입력 페이지에 컴포넌트 추가

--- src/pages/userSettings/UserSettings.jsx
+++ src/pages/userSettings/UserSettings.jsx
@@ -1,5 +1,7 @@
+import { SettingForm } from '../../components/settingForm/SettingForm';
+
 function UserSettings() {
-  return <></>;
+  return <SettingForm title={'정보 입력'} />;
 }
 
 export default UserSettings;
