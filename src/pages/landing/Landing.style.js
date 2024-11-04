3f512941ca6d05d762e02ecb3918745a5c061c0b
Whatdoyumin <lym4963@naver.com>
2024-11-04 17:06:33 +0900

    style: 랜딩 페이지 스타일링 적용

---
+++ src/pages/landing/Landing.style.js
@@ -0,0 +1,105 @@
+import { styled } from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  padding: calc(var(--size-header) + 0.5rem) var(--size-side-gap)
+    calc(var(--size-navbar) + 0.5rem) var(--size-side-gap);
+  background-color: var(--color-blue-700);
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  color: white;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+const Logo = styled.div`
+  font-size: 40px;
+  font-weight: 700;
+`;
+
+const Slogan = styled.p`
+  font-size: 16px;
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  width: 300px;
+  position: absolute;
+  bottom: 30px;
+  color: white;
+  text-align: center;
+`;
+
+const LoginTitle = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  font-size: 14px;
+
+  &::before,
+  &::after {
+    content: '';
+    flex: 1;
+    border-bottom: 1px solid white;
+    margin: 0 10px;
+  }
+`;
+
+const KakaoLoginButton = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  background-color: var(--color-kakao-yellow);
+  color: #000000;
+  font-size: 16px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: bold;
+  text-decoration: none;
+
+  svg {
+    margin-right: 4px;
+  }
+`;
+
+const GuestLogin = styled(Link)`
+  width: 100%;
+  font-size: 14px;
+  color: white;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+`;
+
+export {
+  Container,
+  LogoContainer,
+  Logo,
+  Slogan,
+  LoginContainer,
+  LoginTitle,
+  KakaoLoginButton,
+  GuestLogin,
+};
