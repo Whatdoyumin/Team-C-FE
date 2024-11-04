a7983bd90f5029fa0af4e8e90f5c0d482ba59945
Whatdoyumin <lym4963@naver.com>
2024-11-04 17:06:16 +0900

    feat: 랜딩 페이지 구현

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,5 +1,24 @@
+import * as S from './Landing.style';
+import { RiKakaoTalkFill } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
+
 function Landing() {
-  return <></>;
+  return (
+    <S.Container>
+      <S.LogoContainer>
+        <S.Logo>청년돋움</S.Logo>
+        <S.Slogan>청년들의 사회 적응 길라잡이</S.Slogan>
+      </S.LogoContainer>
+      <S.LoginContainer>
+        <S.LoginTitle>간편 로그인</S.LoginTitle>
+        <S.KakaoLoginButton to="/settings">
+          <RiKakaoTalkFill size={'1.5rem'} />
+          카카오 로그인
+        </S.KakaoLoginButton>
+        <S.GuestLogin to="/home">비회원으로 이용하기</S.GuestLogin>
+      </S.LoginContainer>
+    </S.Container>
+  );
 }
 
 export default Landing;
