120dffce53332448a174bb4b8125b991ce21becd
Whatdoyumin <lym4963@naver.com>
2024-11-10 01:15:59 +0900

    feat: 마이페이지 북마크 컴포넌트 추가

--- src/pages/mypage/Mypage.jsx
+++ src/pages/mypage/Mypage.jsx
@@ -1,10 +1,13 @@
+import { MyBookmark } from '../../components/myBookmark/MyBookmark';
 import { MyProfile } from '../../components/myProfile/MyProfile';
+import * as S from './Mypage.style';
 
 function MyPage() {
   return (
-    <>
+    <S.Container>
       <MyProfile />
-    </>
+      <MyBookmark />
+    </S.Container>
   );
 }
 
