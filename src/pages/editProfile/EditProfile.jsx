468a542e918a1b58f90e1eafb11a8a113dc3e83c
Whatdoyumin <lym4963@naver.com>
2024-11-11 18:30:22 +0900

    feat: 파일명 변경 및 설정 폼 컴포넌트 추가

--- src/pages/editProfile/EditProfile.jsx
+++ src/pages/editProfile/EditProfile.jsx
@@ -1,7 +1,8 @@
 import * as S from './EditProfile.style';
+import { SettingForm } from '../../components/settingForm/SettingForm';
 
 function EditProfile() {
-  return <>정보 수정</>;
+  return <SettingForm title={'정보 수정'} btnText={'수정하기'} full={false} />;
 }
 
 export default EditProfile;
