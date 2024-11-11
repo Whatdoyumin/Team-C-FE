661552e24509d822426b5edf176c3dea2f78259e
Whatdoyumin <lym4963@naver.com>
2024-11-11 18:29:06 +0900

    style: root-layout 스타일 변경

--- src/layout/root-layout.style.js
+++ src/layout/root-layout.style.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  width: 100%;
+  height: 100vh;
   display: flex;
   flex-direction: column;
+  overflow: hidden;
 `;
 
 export { Container };
