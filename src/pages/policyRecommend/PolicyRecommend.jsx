d03687364fd62d0c738e0efdf70f22bf15025d73
Whatdoyumin <lym4963@naver.com>
2024-11-25 15:32:13 +0000

    feat: 미로그인 시 리다이렉트 추가

--- src/pages/policyRecommend/PolicyRecommend.jsx
+++ src/pages/policyRecommend/PolicyRecommend.jsx
@@ -3,26 +3,16 @@ import PolicyList from '../../components/policyList/policyList';
 import { useState } from 'react';
 import userInfo from '../../mocks/userData.json';
 const user = userInfo[0];
-console.log(user);
+
 function PolicyRec() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
+  !isLogin && alert('로그인이 필요한 서비스입니다');
+  window.location.href = '/home';
 
   return (
     <S.Container>
       <S.PolicyContainer>
-        {isLogin ? (
-          <PolicyList isLogin={isLogin} {...user}></PolicyList>
-        ) : (
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              marginTop: '50px',
-            }}
-          >
-            로그인이 필요한 서비스 입니다
-          </div>
-        )}
+        <PolicyList isLogin={isLogin} {...user}></PolicyList>
       </S.PolicyContainer>
     </S.Container>
   );
