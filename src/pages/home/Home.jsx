242c7642957cf0592d2b5fb4a7a08a0298ad1f7b
Whatdoyumin <lym4963@naver.com>
2024-12-06 10:35:34 +0000

    fix: 오류 수정, 필요없는 코드 삭제

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,45 +1,38 @@
 import * as S from './Home.style';
 import PolicyListLogin from '../../components/policyList/policyList';
 import PolicyList from '../../components/policyList/notLogin/policyList';
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState } from 'react';
 import Banner from '../../components/banner/banner';
 import { updateVh } from '../../utils/calculateVH';
 import { LoginContext } from '../../context/LoginContext';
+import { useEffect } from 'react';
 
 const Home = () => {
   const { isLogin } = useContext(LoginContext);
-  const [nickName, setNickName] = useState(null);
-
-  useEffect(() => {
-    updateVh();
-    window.addEventListener('resize', updateVh);
-
-    // Cleanup the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('resize', updateVh);
-    };
-  }, []);
+  const [nickname, setNickname] = useState('');
+  updateVh();
+  window.addEventListener('resize', updateVh);
 
   useEffect(() => {
     if (isLogin) {
       const storedNickName = localStorage.getItem('nickName');
-      setNickName(storedNickName);
+      setNickname(storedNickName);
     }
-  }, [isLogin]); // Only run when isLogin changes
+  }, [isLogin]);
 
   return (
     <S.Container>
-      <Banner />
+      <Banner></Banner>
       <S.PolicyContainer>
         {isLogin ? (
           <>
-            <S.Title>✨ {nickName}님을 위한 추천정책</S.Title>
-            <PolicyListLogin />
+            <S.Title>✨ {nickname.split('의')[0]}님을 위한 추천정책</S.Title>
+            <PolicyListLogin></PolicyListLogin>
           </>
         ) : (
           <>
             <S.Title>🎯 랜덤 정책 추천</S.Title>
-            <PolicyList />
+            <PolicyList></PolicyList>
           </>
         )}
       </S.PolicyContainer>
