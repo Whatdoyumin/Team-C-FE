768621df6e817b0fcc7e31991c277dd4afee117a
Whatdoyumin <lym4963@naver.com>
2024-12-08 04:59:35 +0000

    fix: banner 데이터 객체 배열로 변경

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -20,53 +20,50 @@ const Banner = () => {
     autoplaySpeed: 5000,
   };
 
+  const banners = [
+    {
+      title: '주택청약',
+      content: '청약통장에 가입한 사람에 한하여 아파트 분양 시 당첨자를 선정',
+      img: img1,
+      alt: '주택청약',
+    },
+    {
+      title: '청년도약계좌',
+      content: '월 70만원을 5년 납입하면 약 5,000만 원을 드려요!',
+      img: img2,
+      alt: '청년도약계좌',
+    },
+    {
+      title: 'N분위란?',
+      content: '소득이나 비율을 기준으로 전체를 나누어 분류하는 방법',
+      img: img3,
+      alt: 'N분위란',
+    },
+    {
+      title: '국가장학금',
+      content: '등록금 부담을 줄이기 위해 소득 수준에 따라 차등 지원',
+      img: img4,
+      alt: '국가장학금',
+    },
+    {
+      title: '내일배움카드',
+      content: '직업 능력을 향상시키기 위해 국가에서 훈련비를 지원하는 제도',
+      img: img5,
+      alt: '내일배움카드',
+    },
+  ];
+
   return (
     <S.StyledSlider {...settings}>
-      <S.Banner>
-        <S.Texts>
-          <S.Title>주택청약</S.Title>
-          <S.Content>
-            청약통장에 가입한 사람에 한하여 아파트 분양 시 당첨자를 선정
-          </S.Content>
-        </S.Texts>
-        <S.Img src={img1} alt="주택청약" />
-      </S.Banner>
-      <S.Banner>
-        <S.Texts>
-          <S.Title>청년도약계좌</S.Title>
-          <S.Content>
-            월 70만원을 5년 납입하면 약 5,000만 원을 드려요!
-          </S.Content>
-        </S.Texts>
-        <S.Img src={img2} alt="청년도약계좌" />
-      </S.Banner>
-      <S.Banner>
-        <S.Texts>
-          <S.Title>N분위란?</S.Title>
-          <S.Content>
-            소득이나 비율을 기준으로 전체를 나누어 분류하는 방법
-          </S.Content>
-        </S.Texts>
-        <S.Img src={img3} alt="N분위란" />
-      </S.Banner>
-      <S.Banner>
-        <S.Texts>
-          <S.Title>국가장학금</S.Title>
-          <S.Content>
-            등록금 부담을 줄이기 위해 소득 수준에 따라 차등 지원
-          </S.Content>
-        </S.Texts>
-        <S.Img src={img4} alt="국가장학금" />
-      </S.Banner>
-      <S.Banner>
-        <S.Texts>
-          <S.Title>내일배움카드</S.Title>
-          <S.Content>
-            직업 능력을 향상시키기 위해 국가에서 훈련비를 지원하는 제도
-          </S.Content>
-        </S.Texts>
-        <S.Img src={img5} alt="내일배움카드" />
-      </S.Banner>
+      {banners.map((banner, index) => (
+        <S.Banner key={index}>
+          <S.Texts>
+            <S.Title>{banner.title}</S.Title>
+            <S.Content>{banner.content}</S.Content>
+          </S.Texts>
+          <S.Img src={banner.img} alt={banner.alt} />
+        </S.Banner>
+      ))}
     </S.StyledSlider>
   );
 };
