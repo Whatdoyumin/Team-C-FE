c8edbc4f09c3363dba771243cbf681639226e546
Whatdoyumin <lym4963@naver.com>
2024-12-17 02:53:49 +0000

    feat: 랜딩페이지 수정 전으로 변경

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -6,12 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import logo_wg from '../../images/logo_wg.svg';
 import Portal from './../../components/Portal';
 import { updateVh } from '../../utils/calculateVH';
-import { useEffect } from 'react';
 function requestPermission() {
-  useEffect(() => {
-    updateVh();
-    window.addEventListener('resize', updateVh);
-  }, []);
   // Notification 지원 여부 확인
   if (typeof Notification === 'undefined') {
     console.warn('이 브라우저는 알림을 지원하지 않습니다.');
