02d9e581883d591506b18b48a0f9ee265388cb2d
Whatdoyumin <lym4963@naver.com>
2024-12-17 12:11:54 +0000

    chore 게시글 수정 input 스타일 수정

--- src/components/comment/CommentInput.style.js
+++ src/components/comment/CommentInput.style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const CommentInputBox = styled.div`
   display: flex;
+  justify-content: center;
   margin: 10px 0;
   width: 100%;
   position: fixed;
@@ -13,19 +14,19 @@ const CommentInputBox = styled.div`
 const CommentInput = styled.div`
   position: relative;
   width: 100%;
+  max-width: 600px;
+  min-width: 300px;
+  padding: 0 10px;
 
   input {
-    width: calc(100% - 40px); /* 좌우 마진(20px씩)을 제외한 너비 */
-    max-width: 600px; /* 최대 너비 설정 */
-    min-width: 300px; /* 최소 너비 설정 */
+    width: 100%;
     height: 40px;
     padding: 0 15px;
-    margin: 0 20px; /* 수평 중앙 정렬 */
     border: none;
     border-radius: 20px;
     background-color: #f1f1f1;
     font-size: 13px;
-    box-sizing: border-box; /* padding과 border를 포함한 너비 계산 */
+    box-sizing: border-box;
 
     &:focus {
       border: 1px solid #888;
